2c29ef5de9b32a55bbe00b30d365c2520c9c0fbf
lukarisek <119533638+lukarisek@users.noreply.github.com>
2022-12-03 17:53:41 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,34 @@
+let termometar = 0
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    basic.showNumber(input.temperature())
+    basic.pause(500)
+})
+basic.forever(function () {
+    termometar = input.temperature()
+    if (termometar < 19) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+    } else if (termometar >= 19 && termometar < 22) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            . . . . .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            . . . . .
+            . . . . .
+            `)
+    }
+})
